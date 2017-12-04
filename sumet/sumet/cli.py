ec9759cd58b4168b92d7a3cd724a6824d3e956fe
Asumet120840 <sumet.sa.59@ubu.ac.th>
2017-12-05 00:28:57 +0700

    linux-cli

--- sumet/sumet/cli.py
+++ sumet/sumet/cli.py
@@ -1,6 +1,7 @@
 import click
 import requests
 
+from bs4 import BeautifulSoup
 from PIL import Image
 from StringIO import StringIO
 
@@ -9,7 +10,14 @@ from StringIO import StringIO
 @click.argument('name', default='100013455674701', required=False)
 def main(name, as_cowboy):
     	"""get image on facebook"""
-	graph='https://graph.facebook.com/{}/picture?type=large'.format(name)
+	if 0<=ord(name[0])<=9:
+		graph='https://graph.facebook.com/{}/picture?type=large'.format(name)
+	else:
+		url='https://www.facebook.com/{}'.format(name)
+		html=requests.get(url)
+		b=BeautifulSoup(html.content,'html.parser')
+		Facebook_id=b.find_all('meta')[7]['content'].replace('fb://profile/','')
+		graph='https://graph.facebook.com/{}/picture?type=large'.format(Facebook_id)
     	req=requests.get(graph)
     	img=Image.open(StringIO(req.content))
     	img.show()
